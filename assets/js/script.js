2d1d556433daded36060b5f18a302e68c19b93b5
Tom Dent <thomasdent1993@hotmail.co.uk>
2024-02-24 20:16:10 +0000

    Created some javascript to allow a quiz rules pop up box to show up when start quiz button clicked. Also created a button to start the quiz and exit the quiz with corresponding javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const openQuizBtn = document.querySelector('.open-quiz');
 const quizRules = document.querySelector('.quiz-rules');
 const exitQuizBtn = document.querySelector('.exit-quiz');
 const startScreen = document.querySelector('.start-screen');
+const startQuizBtn = document.querySelector('.start-quiz');
+const quizArea = document.querySelector('.quiz-area');
 
 openQuizBtn.onclick = ()=> {
     quizRules.classList.add('active');
@@ -12,4 +14,9 @@ openQuizBtn.onclick = ()=> {
 exitQuizBtn.onclick = ()=> {
     quizRules.classList.remove('active');
     startScreen.classList.remove('active');
+}
+
+startQuizBtn.onclick = ()=> {
+    quizArea.classList.add('active');
+    quizRules.classList.remove('active');
 }
