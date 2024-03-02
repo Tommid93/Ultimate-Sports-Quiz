ac84ff0a09534785af6971cc7dc059067d6e3f2a
Tom Dent <thomasdent1993@hotmail.co.uk>
2024-03-02 16:53:59 +0000

    Created js to run through questions and correct answers when next button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,9 +27,23 @@ startQuizBtn.onclick = ()=> {
 
 let que_count = 0;
 
+const next_ques = quizArea.querySelector(".next-ques");
+
+/* When next question button clicked - will move to next question */
+next_ques.onclick = ()=> {
+    que_count++;
+    showQuestions(que_count);
+}
+
 /* Function to get questions from array */
 function showQuestions(index){
     const que_text = document.querySelector(".questions");
+    const answer_options = document.querySelector(".answer-options");
     let que_tag = '<span>'+ questions[index].question;
+    let answer_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[3] +'<span></span></div>';
     que_text.innerHTML = que_tag;
+    answer_options.innerHTML = answer_tag;
 }
