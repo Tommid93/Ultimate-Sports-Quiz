c18f11f8e8bf590b4410125990ed8f03ff2d10f0
Tom Dent <thomasdent1993@hotmail.co.uk>
2024-03-02 15:33:39 +0000

    Created a second javascript file that will have all my questions. This will be a large document so felt it was necessary to separate it from the functional js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,20 @@ const startScreen = document.querySelector('.start-screen');
 const startQuizBtn = document.querySelector('.start-quiz');
 const quizArea = document.querySelector('.quiz-area');
 
+/* Start Quiz button - once clicked will open the quiz rules */
 openQuizBtn.onclick = ()=> {
     quizRules.classList.add('active');
     startScreen.classList.add('active');
 
 }
 
+/* When quiz rules is open - if click exit button will close quiz rules and return to start button */
 exitQuizBtn.onclick = ()=> {
     quizRules.classList.remove('active');
     startScreen.classList.remove('active');
 }
 
+/* When quiz rules is open - if click start quiz button, will close quiz rules and open the quiz */
 startQuizBtn.onclick = ()=> {
     quizArea.classList.add('active');
     quizRules.classList.remove('active');
