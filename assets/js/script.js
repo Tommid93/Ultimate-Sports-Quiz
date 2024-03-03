4669e4a994976c53caaf99841b59f0560833de0c
Tom Dent <thomasdent1993@hotmail.co.uk>
2024-03-03 13:01:18 +0000

    Created JS to indicate correct answer if user has selected incorrectly. Also written js to disable all options once user has selected an option

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const startScreen = document.querySelector('.start-screen');
 const startQuizBtn = document.querySelector('.start-quiz');
 const quizArea = document.querySelector('.quiz-area');
 
+const answer_options = document.querySelector(".answer-options");
+
 /* Start Quiz button - once clicked will open the quiz rules */
 openQuizBtn.onclick = ()=> {
     quizRules.classList.add('active');
@@ -47,7 +49,6 @@ next_ques.onclick = ()=> {
 /* Function to get questions from array */
 function showQuestions(index){
     const que_text = document.querySelector(".questions");
-    const answer_options = document.querySelector(".answer-options");
     let que_tag = '<span>'+ questions[index].question;
     let answer_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                     +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
@@ -63,15 +64,28 @@ function showQuestions(index){
     }   
 }
 
+/* Function to identify if the user selects the correct or incorrect answer */
 function optionSelected(answer){
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
+    let allOptions = answer_options.children.length;
     if(userAns ==correctAns){
         console.log("Correct");
         answer.classList.add("correct");
     }else{
         console.log("Incorrect");
         answer.classList.add("incorrect");
+    }
+
+/* If user selects the incorrect answer, will automatically show them the correct answer */
+    for (let i = 0; i < allOptions; i++) {
+        if(answer_options.children[i].textContent == correctAns){
+            answer_options.children[i].setAttribute("class", "option correct");
+        }
+    }
+/* Will disable all options once a user has selcted their choice of answer */
+    for (let i = 0; i < allOptions; i++){
+        answer_options.children[i].classList.add("disabled");
     }
 }
 
