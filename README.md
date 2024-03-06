75cdfdf3f7550956f22c48ac139d435f80864558
Tom Dent <thomasdent1993@hotmail.co.uk>
2024-03-06 21:47:28 +0000

    Complete some further styling on larger screens and begin my readme

--- README.md
+++ README.md
@@ -1,39 +1,14 @@
 ![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
-Welcome,
+### The Ultimate Sports Quiz
 
-This is the Code Institute student template for Codeanywhere. If you are using Gitpod then you need [this template](https://github.com/Code-Institute-Org/gitpod-full-template) instead.  We have preinstalled all of the tools you need to get started. It's perfectly ok to use this template as the basis for your project submissions.
+## Introduction
+Before I explain my project I wanted to point out that I had already completed 6 months of learning on Javascript before being this course. The interest I had in this is what inspired me to want to take this further. I bring this up as I feel that my coding for this project is a little bit more advanced than a beginner. I came into this project with some good knowledge compared to other types of coding such as Python to which I have no knowledge. 
 
-You can safely delete this README.md file, or change it for your own project. Please do read it at least once, though! It contains some important information about Codeanywhere and the extensions we use. Some of this information has been updated since the video content was created. The last update to this file was: **August 30th, 2023**
+I used one of the ideas in the assessment criteria for this project which was to design a quiz game. I felt that there were plenty of features that I could implement that would demonstrate my knowledge of javascript but that would still test me. I wanted the quiz game to be challenging but fun at the same time.
 
-## Codeanywhere Reminders
+# Target Audience
+As the name of my project suggests, my target audience for this project is passionate or knowledgable sports fans who wanted to test their knowledge. Originally I was going to complete a general knowledge test, however I decided I wanted to have a more defined target audience which would also help me to create a more specialised design for the project rather than being generic.
 
-To run a frontend (HTML, CSS, Javascript only) application in Codeanywhere, in the terminal, type:
+#
 
-`python3 -m http.server`
-
-A button should appear to click: _Open Preview_ or _Open Browser_.
-
-To run a frontend (HTML, CSS, Javascript only) application in Codeanywhere with no-cache, you can use this alias for `python3 -m http.server`.
-
-`http_server`
-
-To run a backend Python file, type `python3 app.py`, if your Python file is named `app.py` of course.
-
-A button should appear to click: _Open Preview_ or _Open Browser_.
-
-In Codeanywhere you have superuser security privileges by default. Therefore you do not need to use the `sudo` (superuser do) command in the bash terminal in any of the lessons.
-
-To log into the Heroku toolbelt CLI:
-
-1. Log in to your Heroku account and go to _Account Settings_ in the menu under your avatar.
-2. Scroll down to the _API Key_ and click _Reveal_
-3. Copy the key
-4. In Codeanywhere, from the terminal, run `heroku_config`
-5. Paste in your API key when asked
-
-You can now use the `heroku` CLI program - try running `heroku apps` to confirm it works. This API key is unique and private to you so do not share it. If you accidentally make it public then you can create a new one with _Regenerate API Key_.
-
----
-
-Happy coding!
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,78 +6,53 @@ const startQuizBtn = document.querySelector('.start-quiz');
 const quizArea = document.querySelector('.quiz-area');
 const timeCount = quizArea.querySelector('.time-seconds');
 
-const answer_options = document.querySelector(".answer-options");
+const answerOptions = document.querySelector(".answer-options");
 
-/* Start Quiz button - once clicked will open the quiz rules */
-openQuizBtn.onclick = ()=> {
-    quizRules.classList.add('active');
-    startScreen.classList.add('active');
-
-}
-
-/* When quiz rules is open - if click exit button will close quiz rules and return to start button */
-exitQuizBtn.onclick = ()=> {
-    quizRules.classList.remove('active');
-    startScreen.classList.remove('active');
-}
-
-/* When quiz rules is open - if click start quiz button, will close quiz rules and open the quiz */
-startQuizBtn.onclick = ()=> {
-    quizArea.classList.add('active');
-    quizRules.classList.remove('active');
-    showQuestions(0);
-    queCounter(1);
-    timerBegin(10);
-}
-
-let que_count = 0;
-let que_numb = 1;
+let questionCount = 0;
+let questionNumb = 1;
 let counter;
 let timeStart = 10;
 let userScore = 0;
 
-const next_ques = quizArea.querySelector(".next-ques");
-const result_area = document.querySelector(".results");
-const exit_quiz = result_area.querySelector(".exit-quiz");
+const nextQuestion = quizArea.querySelector(".next-ques");
+const resultArea = document.querySelector(".results");
+const exitQuiz = resultArea.querySelector(".exit-quiz");
 
 /* Exit button at end of quiz will take user to the main page */
-exit_quiz.onclick = () =>{
+exitQuiz.onclick = () =>{
     window.location.reload();
 }
 
 
 /* When next question button clicked - will move to next question */
-next_ques.onclick = ()=> {
-    if(que_count < questions.length - 1){
-    que_count++;
-    que_numb++;
-    showQuestions(que_count);
-    queCounter(que_numb);
+nextQuestion.onclick = ()=> {
+    if(questionCount < questions.length - 1){
+    questionCount++;
+    questionNumb++;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
     clearInterval(counter);
     timerBegin(10);
-    next_ques.style.display = "none";
+    nextQuestion.style.display = "none";
     
     }else{
-        console.log("Questions Completed")
         showResultBox();
     }
 }
 
 /* Function to get questions from array */
 function showQuestions(index){
-    const que_text = document.querySelector(".questions");
-    let que_tag = '<span>'+ questions[index].question;
-    let answer_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+    const questionText = document.querySelector(".questions");
+    let questionTag = '<span>'+ questions[index].question;
+    let answerTag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                     +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
                     +'<div class="option">'+ questions[index].options[2] +'<span></span></div>'
                     +'<div class="option">'+ questions[index].options[3] +'<span></span></div>';
-    que_text.innerHTML = que_tag;
-    answer_options.innerHTML = answer_tag;
-    const option = answer_options.querySelectorAll(".option");
-    console.log(option);
+    questionText.innerHTML = questionTag;
+    answerOptions.innerHTML = answerTag;
+    const option = answerOptions.querySelectorAll(".option");
     for (let i = 0; i < option.length; i++) {
        option[i].setAttribute("onclick", "optionSelected(this)");
-       console.log(option[i]);
     }   
 }
 
@@ -85,28 +60,25 @@ function showQuestions(index){
 function optionSelected(answer){
     clearInterval(counter);
     let userAns = answer.textContent;
-    let correctAns = questions[que_count].answer;
-    let allOptions = answer_options.children.length;
+    let correctAns = questions[questionCount].answer;
+    let allOptions = answerOptions.children.length;
     if(userAns ==correctAns){
         userScore += 1;
-        console.log(userScore);
-        console.log("Correct");
         answer.classList.add("correct");
     }else{
-        console.log("Incorrect");
         answer.classList.add("incorrect");
     }
 
 /* If user selects the incorrect answer, will automatically show them the correct answer */
     for (let i = 0; i < allOptions; i++) {
-        if(answer_options.children[i].textContent == correctAns){
-            answer_options.children[i].setAttribute("class", "option correct");
+        if(answerOptions.children[i].textContent == correctAns){
+            answerOptions.children[i].setAttribute("class", "option correct");
         }
     }
 /* Will disable all options once a user has selcted their choice of answer */
     for (let i = 0; i < allOptions; i++){
-        answer_options.children[i].classList.add("disabled");
-        next_ques.style.display = "block";
+        answerOptions.children[i].classList.add("disabled");
+        nextQuestion.style.display = "block";
     }
         
 }
@@ -115,11 +87,12 @@ function optionSelected(answer){
 function showResultBox(){
     quizArea.classList.remove('active');
     quizRules.classList.remove('active');
-    result_area.classList.add('active');
-    const score = result_area.querySelector('.results-text');
+    resultArea.classList.add('active');
+    const score = resultArea.querySelector('.results-text');
 
     let scoreTag = '<p>You scored</p>'+ userScore +'<p>Out of 15 Questions</p>';
     score.innerHTML = scoreTag;
+    trySetHighestScore();
 }
 
 /* Timer function that will also force the user to skip to the next question if the timer runs out*/
@@ -132,25 +105,64 @@ function timerBegin(time){
             clearInterval(counter);
             timeCount.textContent = "0"; 
 
-            let correctAns = questions[que_count].answer;
-            let allOptions = answer_options.children.length;
+            let correctAns = questions[questionCount].answer;
+            let allOptions = answerOptions.children.length;
 
             for (let i = 0; i < allOptions; i++) {
-                if(answer_options.children[i].textContent == correctAns){
-                    answer_options.children[i].setAttribute("class", "option correct");
+                if(answerOptions.children[i].textContent == correctAns){
+                    answerOptions.children[i].setAttribute("class", "option correct");
                 }
             }
             for (let i = 0; i < allOptions; i++){
-                answer_options.children[i].classList.add("disabled");
-                next_ques.style.display = "block";
+                answerOptions.children[i].classList.add("disabled");
+                nextQuestion.style.display = "block";
             }
         }
     }
 }
 
 /* Function to change question counter in footer */
-function queCounter(index){
-const bottom_ques_counter = quizArea.querySelector(".current-ques");
-let totalQuesCountTag = '<p class="current-ques">'+ index +'</p><p>Of 15 Questions</p>';
-bottom_ques_counter.innerHTML = totalQuesCountTag;
+function questionCounter(index){
+const bottomQuestionCounter = quizArea.querySelector(".current-ques");
+let totalQuestionCountTag = '<p class="current-ques">'+ index +'</p><p>Of 15 Questions</p>';
+bottomQuestionCounter.innerHTML = totalQuestionCountTag;
 }
+
+    
+ /* Start Quiz button - once clicked will open the quiz rules */
+openQuizBtn.onclick = ()=> {
+    quizRules.classList.add('active');
+    startScreen.classList.add('active');
+
+}
+/* When quiz rules is open - if click exit button will close quiz rules and return to start button */
+exitQuizBtn.onclick = ()=> {
+    quizRules.classList.remove('active');
+    startScreen.classList.remove('active');
+}
+
+/* When quiz rules is open - if click start quiz button, will close quiz rules and open the quiz */
+startQuizBtn.onclick = ()=> {
+    quizArea.classList.add('active');
+    quizRules.classList.remove('active');
+    showQuestions(0);
+    questionCounter(1);
+    timerBegin(10);
+
+    }
+
+/* Function to set a high score and update it depending on users score*/
+    let high_score = 0;
+    let previousHigh = 0;
+    document.querySelector(".high-score").textContent = previousHigh;
+    function trySetHighestScore() {
+        console.log(previousHigh)
+        console.log(userScore)
+        previousHighString = localStorage.getItem("high-score")
+        previousHigh = previousHighString ? parseInt(previousHighString) : 0;
+      
+        if (userScore > previousHigh) {
+          localStorage.setItem("high-score", userScore);
+        }
+      }
+
